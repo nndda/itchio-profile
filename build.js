71f296f6ac94a53ab3c78882a19e714bec269b75
nnda <96333146+nndda@users.noreply.github.com>
2024-10-10 22:31:48 +0800

    Fix filenames

--- build.js
+++ build.js
@@ -3,13 +3,13 @@ const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
 
-child_process.exec("sass --no-source-map \\!custom-css.scss custom-css.css && cleancss -O2 --output custom-css.min.css custom-css.css", (error) => {
+child_process.exec("sass --no-source-map styles.scss styles.css && cleancss -O2 --output styles.min.css styles.css", (error) => {
 
   if (error) {
     console.error(error);
 
   } else {
-    const cssMin = fs.readFileSync(path.resolve(__dirname, "custom-css.min.css")).toString();
+    const cssMin = fs.readFileSync(path.resolve(__dirname, "styles.min.css")).toString();
 
     const regexComment = /\/\*! IMPORT-START \*\/(.*?)\/\*! IMPORT-END \*\//g;
 
@@ -22,8 +22,8 @@ child_process.exec("sass --no-source-map \\!custom-css.scss custom-css.css && cl
       CSSDirect = CSSDirect.replace(match[0], "");
     }
 
-    fs.writeFileSync(path.resolve(__dirname, "custom-css-direct.min.css"), CSSDirect);
-    fs.writeFileSync(path.resolve(__dirname, "custom-css-import.min.css"), CSSImported.join(""));
+    fs.writeFileSync(path.resolve(__dirname, "dist/styles-direct.min.css"), CSSDirect);
+    fs.writeFileSync(path.resolve(__dirname, "dist/styles-import.min.css"), CSSImported.join(""));
 
   }
 
