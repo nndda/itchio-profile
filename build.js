552a8c04c01f77f423d1559113ab5ae61e866229
nnda <96333146+nndda@users.noreply.github.com>
2024-10-10 23:28:44 +0800

    Hash output filename

--- build.js
+++ build.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const path = require("path");
 const child_process = require("child_process");
+const crypto = require("crypto");
 
 child_process.exec("sass --no-source-map styles.scss styles.css && cleancss -O2 --output styles.min.css styles.css", (error) => {
 
@@ -22,9 +23,21 @@ child_process.exec("sass --no-source-map styles.scss styles.css && cleancss -O2 
       CSSDirect = CSSDirect.replace(match[0], "");
     }
 
+    let CSSImportedStr = CSSImported.join("");
+
+    const hash = crypto.createHash("sha256");
+    hash.update(CSSImportedStr);
+    const hashFilename = hash.digest("hex").slice(0, 8);
+
+    const distDir = path.resolve(__dirname, "dist/");
+
+    fs.readdirSync(distDir).forEach(value => {
+      fs.unlinkSync(path.resolve(distDir, value));
+    });
+
     fs.writeFileSync(path.resolve(__dirname, "dist/styles-direct.min.css"),
-      `@import url("https://cdn.jsdelivr.net/gh/nndda/itchio-profile@main/dist/styles-import.min.css");` + CSSDirect);
-    fs.writeFileSync(path.resolve(__dirname, "dist/styles-import.min.css"), CSSImported.join(""));
+      `@import url("https://cdn.jsdelivr.net/gh/nndda/itchio-profile@main/dist/${hashFilename}.css");` + CSSDirect);
+    fs.writeFileSync(path.resolve(__dirname, `dist/${hashFilename}.css`), CSSImportedStr);
 
   }
 
