501677b1a7da01d937ce295bf308605d1913d05f
nnda <96333146+nndda@users.noreply.github.com>
2024-10-18 04:18:48 +0800

    Use release

--- build.js
+++ build.js
@@ -8,7 +8,8 @@ const autoprefixer = require("autoprefixer");
 const path = require("path");
 const crypto = require("crypto");
 
-const CDNImportTemplate = `@import url("https://cdn.jsdelivr.net/gh/nndda/itchio-profile@main/dist/%s.css");`;
+const version = "1.0.0";
+const CDNImportTemplate = `@import url("https://cdn.jsdelivr.net/gh/nndda/itchio-profile@${version}/dist/%s.css");`;
 const maxCharLimit = 5120 - CDNImportTemplate.length - 8;
 
 // Process CSS ============================================================================
