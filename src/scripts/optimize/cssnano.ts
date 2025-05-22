1e3ecd34170f41a7d9132cf239d2c9ab572972b8
nnda <96333146+nndda@users.noreply.github.com>
2025-05-22 16:27:38 +0700

    Set `reduceIdents` as an optional parameter

--- src/scripts/optimize/cssnano.ts
+++ src/scripts/optimize/cssnano.ts
@@ -2,17 +2,20 @@ import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 
 import cssnano from "cssnano";
-import cssnanoPresetAdvanced from "cssnano-preset-advanced";
-const preset = cssnanoPresetAdvanced(
-  {
-    discardOverridden: false,
-    discardUnused: false,
-  }
-);
+import cssnanoPresetAdvanced, { type SimpleOptions } from "cssnano-preset-advanced";
 
 export default async function (
   src: string,
+  reduceIdents: boolean = false,
 ): Promise<string> {
+  const preset = cssnanoPresetAdvanced(
+    {
+      discardOverridden: false,
+      discardUnused: false,
+      reduceIdents: reduceIdents as SimpleOptions,
+    }
+  );
+
   return (
     await postcss([cssnano({ preset, plugins: [autoprefixer] })])
       .process(src, { from: undefined })
