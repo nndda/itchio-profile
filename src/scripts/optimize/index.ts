b7322b273d88174a64b504746649877fbe82241e
nnda <96333146+nndda@users.noreply.github.com>
2025-05-22 18:33:33 +0700

    Remove unused import

--- src/scripts/optimize/index.ts
+++ src/scripts/optimize/index.ts
@@ -1,9 +1,7 @@
-import fnAnim from "./anim";
 import fnCol from "./col";
 import fnProp from "./prop";
 import fnNano from "./cssnano";
 
-export const anim = fnAnim;
 export const col = fnCol;
 export const prop = fnProp;
 export const nano = fnNano;
