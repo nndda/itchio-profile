541447e22987a9b73219d923ae7bb1f1c14dbc61
nnda <96333146+nndda@users.noreply.github.com>
2025-05-22 11:40:49 +0700

    Create main entry for CSS functions

---
+++ src/scripts/optimize/index.ts
@@ -0,0 +1,9 @@
+import fnAnim from "./anim";
+import fnCol from "./col";
+import fnProp from "./prop";
+import fnNano from "./cssnano";
+
+export const anim = fnAnim;
+export const col = fnCol;
+export const prop = fnProp;
+export const nano = fnNano;
