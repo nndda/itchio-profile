dd7aca1b3cbaabeec82e01462d3449766cfcb6b1
nnda <96333146+nndda@users.noreply.github.com>
2025-08-12 14:38:37 +0700

    Implement CDN assets versioning
    Closes #9

---
+++ src/scripts/cdnify.ts
@@ -0,0 +1,16 @@
+// user/repo@main
+// user/repo@version
+
+export default function (
+  str: string,
+  userRepo: string,
+  version: string,
+): string {
+  return str.replace(/https\:\/\/cdn\.jsdelivr\.net\/.+?\/(.+?\/.+?)\@(main)\//g, (
+      match,
+      userRepoMatch,
+      verMainMatch,
+    ): string => {
+    return match.replace(`${userRepoMatch}@${verMainMatch}`, `${userRepo}@${version}`);
+  });
+}
